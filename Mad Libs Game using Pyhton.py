9fc9cb5f429795598fee22ecef4601cbf2b492bb
HiraZulfiqar <142989490+HiraZulfiqar@users.noreply.github.com>
2024-07-07 07:53:00 -0700

    Add files via upload

---
+++ Mad Libs Game using Pyhton.py
@@ -0,0 +1,19 @@
+print("*"*5,"MAD LIB GAME", "*"*5)
+print("-"*5, "THhe Magic Computer", "-"*5)
+
+print("Today, every student has a computer small enough to fit into his _______ ")
+print("He can solve any math problem by simply pushing the computer's little ________ ")
+print("Computer can add, multipy, divide &  __________ ")
+print(" We can also play games on ___ ")
+
+noun_ = input("Enter the noun: ")
+pronoun_ =input("Enter the pronoun: ")
+verb_= input("Enter the verb: ")
+subject_ = input("Enter the subject: ")
+
+print("Today, every student has a computer small enough to fit into his"+ noun_)
+print("He can solve any math problem by simply pushing the computer's little" +pronoun_)
+print("Computer can add, multipy, divide & " +verb_ )
+print(" We can also play games on" +subject_)
+
+               
